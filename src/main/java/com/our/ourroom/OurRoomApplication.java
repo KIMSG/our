27ddb88aa0b38c4de92530eebc4d7a5a2b328f95
±载±â <yuiox17@gmail.com>
2024-12-18 21:09:20 +0900

    boogie on n on

---
+++ src/main/java/com/our/ourroom/OurRoomApplication.java
@@ -0,0 +1,13 @@
+package com.our.ourroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/our/ourroom/OurRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.our.ourroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
