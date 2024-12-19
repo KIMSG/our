d29a58f2159b0bea3f117a04df207aad8664810e
김슬기 <yuiox17@gmail.com>
2024-12-20 00:15:24 +0900

    feat : jdbc 설정 docs : 문서 수정
    프로젝트 수정에 따른 README.md 작성
    - 데이터베이스 연동을 위해서 jdbc 추가
    - h2 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- 회의실 데이터
+INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (1, '회의실 A', 10);
+INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (2, '회의실 B', 15);
+INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (3, '회의실 C', 20);
+
+-- 사용자 데이터
+INSERT INTO USERS (id, name) VALUES (1, '포스트말론');
+INSERT INTO USERS (id, name) VALUES (2, '도자캣');
+INSERT INTO USERS (id, name) VALUES (3, '비버');
