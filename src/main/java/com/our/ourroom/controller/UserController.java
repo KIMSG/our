6a911b48a2ba7eb2529c4348ba7793c52aa97f23
김슬기 <yuiox17@gmail.com>
2024-12-21 15:42:39 +0900

    docs: 스웨거 작성 chore: 스웨거 설정
    - 사용자 생성에 대한 api 작성
    - 부트 버전에 맞추어 스웨거 설정 추가

--- src/main/java/com/our/ourroom/controller/UserController.java
+++ src/main/java/com/our/ourroom/controller/UserController.java
@@ -2,9 +2,14 @@ package com.our.ourroom.controller;
 
 import com.our.ourroom.entity.Users;
 import com.our.ourroom.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "사용자 API", description = "사용자 관리와 관련된 API 제공")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -20,8 +25,14 @@ public class UserController {
      * @param user 사용자 데이터 (JSON)
      * @return 생성된 사용자 데이터
      */
+    @Operation(summary = "사용자 생성", description = "새로운 사용자를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 데이터 반환"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping
-    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+    public ResponseEntity<Users> createUser(
+            @RequestBody Users user) {
         Users createdUser = userService.createUser(user);
         return ResponseEntity.ok(createdUser);
     }
--- src/main/java/com/our/ourroom/entity/Users.java
+++ src/main/java/com/our/ourroom/entity/Users.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 
 /**
  * Users 엔티티
  * - 사용자 정보를 저장하는 엔티티로, 사용자 ID와 이름을 포함합니다.
  */
+@Schema(description = "사용자 정보를 나타내는 엔티티")
 @Entity
 @Getter
 @Setter
@@ -19,6 +22,7 @@ public class Users {
      * 사용자 고유 ID
      * - 자동 생성되는 기본 키 값
      */
+    @Schema(description = "사용자 고유 ID", example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,6 +31,7 @@ public class Users {
      * 사용자 이름
      * - 사용자의 이름을 저장
      */
+    @Schema(description = "사용자 이름", example = "John Doe")
     @Column(nullable = false)
     private String name;
 
