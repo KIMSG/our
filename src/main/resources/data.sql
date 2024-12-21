17b286486f834a4f833926afaa9c58ff51e60443
김슬기 <yuiox17@gmail.com>
2024-12-21 12:00:26 +0900

    chore: 데이터 유지 설정
    - 초기 데이터 입력 이후 실행마다 초기화 금지

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,27 @@
 -- 회의실 데이터
-INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (1, '회의실 A', 10);
-INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (2, '회의실 B', 15);
-INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (3, '회의실 C', 20);
+INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (1, '회의실 A', 3);
+INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (2, '회의실 B', 5);
+INSERT INTO MEETING_ROOM (id, name, capacity) VALUES (3, '회의실 C', 10);
 
 -- 사용자 데이터
 INSERT INTO USERS (id, name) VALUES (1, '포스트말론');
 INSERT INTO USERS (id, name) VALUES (2, '도자캣');
-INSERT INTO USERS (id, name) VALUES (3, '비버');
+INSERT INTO USERS (id, name) VALUES (3, '켄드릭 라마');
+INSERT INTO USERS (id, name) VALUES (4, '트래비스 스캇');
+INSERT INTO USERS (id, name) VALUES (5, '제이 콜');
+INSERT INTO USERS (id, name) VALUES (6, '릴 나스 엑스');
+INSERT INTO USERS (id, name) VALUES (7, '차일디시 감비노');
+INSERT INTO USERS (id, name) VALUES (8, '맥 밀러');
+INSERT INTO USERS (id, name) VALUES (9, '에이셉 라키');
+INSERT INTO USERS (id, name) VALUES (10, '타일러 더 크리에이터');
+INSERT INTO USERS (id, name) VALUES (11, '빌리 아일리시');
+INSERT INTO USERS (id, name) VALUES (12, '리조');
+INSERT INTO USERS (id, name) VALUES (13, '할시');
+INSERT INTO USERS (id, name) VALUES (14, '존 메이어');
+INSERT INTO USERS (id, name) VALUES (15, '찰리 푸스');
+INSERT INTO USERS (id, name) VALUES (16, '제이지');
+INSERT INTO USERS (id, name) VALUES (17, '카디비');
+INSERT INTO USERS (id, name) VALUES (18, '브루노 마스');
+INSERT INTO USERS (id, name) VALUES (19, '셔린 디온');
+INSERT INTO USERS (id, name) VALUES (20, '엘튼 존');
+
